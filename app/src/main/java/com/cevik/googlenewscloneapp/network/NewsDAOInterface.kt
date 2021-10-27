5c6ffc4a532f14567b66f2fe67748ffaa4cf3b7c
Furkan Çevik <57840021+denefc@users.noreply.github.com>
2021-10-27 12:50:19 +0300

    apikey changes

--- app/src/main/java/com/cevik/googlenewscloneapp/network/NewsDAOInterface.kt
+++ app/src/main/java/com/cevik/googlenewscloneapp/network/NewsDAOInterface.kt
@@ -5,7 +5,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface NewsDAOInterface {
-    @GET("everything?q=Apple&from=2021-10-26&sortBy=popularity&apiKey=00fbe46e6c2d4253b6b772ac460d4ceb")
+    @GET("everything?q=Apple&from=2021-10-26&sortBy=popularity&apiKey={YOURAPİKEYSHOULDBEHERE}")
     fun getNews(): Call<NewsResponse>
 
 }
